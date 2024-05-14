15d04b0f951b17e26e922ead2db9296c9a61036a
chriskerrc <chriskerrc@gmail.com>
2024-05-14 19:17:21 +0100

    Store all basic actions

--- src/main/java/edu/uob/GameAction.java
+++ src/main/java/edu/uob/GameAction.java
@@ -1,6 +1,7 @@
 package edu.uob;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class GameAction
 {
@@ -25,6 +26,37 @@ public class GameAction
 
     public void setNarration(String inputNarration){
         narration = inputNarration;
+    }
+
+    public GameEntity getSubjectEntityFromName(String entityName){
+        for(GameEntity gameEntity : subjectEntities){
+            if(Objects.equals(gameEntity.getName(), entityName)){
+                return gameEntity;
+            }
+        }
+        return null;
+    }
+
+    public GameEntity getConsumedEntityFromName(String entityName){
+        for(GameEntity gameEntity : consumedEntities){
+            if(Objects.equals(gameEntity.getName(), entityName)){
+                return gameEntity;
+            }
+        }
+        return null;
+    }
+
+    public GameEntity getProducedEntityFromName(String entityName){
+        for(GameEntity gameEntity : producedEntities){
+            if(Objects.equals(gameEntity.getName(), entityName)){
+                return gameEntity;
+            }
+        }
+        return null;
+    }
+
+    public String getProducedNarration(){
+        return narration;
     }
 
 
